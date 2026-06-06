bdca7e8c79aa8e3903603f0b41cee430eb5b0947
RedStar <redstar071@proton.me>
2026-06-06 22:52:51 +0200

    chore: remove obsolete tsup configuration files from shared-http-pieces and shared-influx-pieces

--- packages/shared-influx-pieces/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { createTsupConfig } from '../../scripts/tsup.config';
-
-export default createTsupConfig({
-	cjsOptions: { disabled: true },
-	esmOptions: {
-		entry: ['src/index.ts', 'src/register.ts'],
-		target: 'es2022'
-	}
-});
