52b3ed1ffe1fbb17afc8d03091dfe70977f905f1
RedStar <redstar071@proton.me>
2026-06-21 14:54:16 +0200

    chore: remove workspace dependency on @wolfstar/http-framework-test-utils and update semver version

--- packages/http-framework/vitest.config.ts
+++ packages/http-framework/vitest.config.ts
@@ -2,13 +2,14 @@ import { fileURLToPath } from 'node:url';
 import { defineProject, mergeConfig } from 'vitest/config';
 import configShared from '../../vitest.shared.js';
 
-export default mergeConfig(
-	configShared,
-	defineProject({
+export default defineProject(
+	mergeConfig(configShared, {
 		esbuild: { target: 'es2021' },
 		resolve: {
 			alias: {
-				'@wolfstar/http-framework': fileURLToPath(new URL('./src/index.ts', import.meta.url))
+				'@wolfstar/http-framework': fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+				'@wolfstar/http-framework-test-utils': fileURLToPath(new URL('../http-framework-test-utils/src/index.ts', import.meta.url)),
+				'@wolfstar/http-framework-test-utils/vitest': fileURLToPath(new URL('../http-framework-test-utils/src/vitest.ts', import.meta.url))
 			}
 		}
 	})
