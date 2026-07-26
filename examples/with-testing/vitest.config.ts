1bc27d6fba85804520055f3d050a49cf156699d9
RedStar071 <redstar071@proton.me>
2026-07-26 16:00:25 +0000

    fix: alias @wolfstar sources in with-testing vitest config

--- examples/with-testing/vitest.config.ts
+++ examples/with-testing/vitest.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'node:url';
 import { defineProject, mergeConfig } from 'vitest/config';
 import configShared from '../../vitest.shared.js';
 
@@ -8,6 +9,22 @@ export default mergeConfig(
 			name: 'with-testing',
 			include: ['tests/**/*.test.ts'],
 			setupFiles: ['./vitest.setup.ts']
+		},
+		resolve: {
+			alias: [
+				{
+					find: '@wolfstar/http-framework-test-utils/vitest',
+					replacement: fileURLToPath(new URL('../../packages/http-framework-test-utils/src/vitest.ts', import.meta.url))
+				},
+				{
+					find: '@wolfstar/http-framework-test-utils',
+					replacement: fileURLToPath(new URL('../../packages/http-framework-test-utils/src/index.ts', import.meta.url))
+				},
+				{
+					find: '@wolfstar/http-framework',
+					replacement: fileURLToPath(new URL('../../packages/http-framework/src/index.ts', import.meta.url))
+				}
+			]
 		}
 	})
 );
